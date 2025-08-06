2d8aba8a466d56ef29ce90611f32591c9cf00d10
temwani <temwaninkowani@gmail.com>
2025-08-06 04:16:29 +0200

    50% complete about page

---
+++ src/app/components/policygrid/page.js
@@ -0,0 +1,34 @@
+import React from "react";
+import InfoCard from "../infocard/page";
+import { Hospital, Lock, Trees } from "lucide-react";
+import styles from "../missiongrid/missiongrid.module.css"
+
+const policies = [
+    {
+        title:"Health And Safety Policy",
+        icon: Hospital
+    },
+    {
+        title:"Environmental Policy",
+        icon:Trees
+    },
+    {
+        title:"Security Policy",
+        icon: Lock
+    }
+];
+
+
+export default function PolicyGrid(){
+    return(
+        <div className={styles.missionGrid}>
+            {policies.map((policy,index) => (
+                <InfoCard
+                key={index}
+                title={policy.title}
+                icon={policy.icon}></InfoCard>
+            ))}
+
+        </div>
+    );
+}
--- src/app/pages/about/page.js
+++ src/app/pages/about/page.js
@@ -2,14 +2,23 @@ import React from "react";
 import styles from "./about.module.css";
 import HomeNav from "@/app/components/navbar/page";
 import Footer from "@/app/components/footer/page";
+import PolicyGrid from "@/app/components/policygrid/page";
+import Button from "@/app/components/button/page";
 
 export default function AboutPage(){
     return(
         <main>
           <HomeNav style={{background : "#1a2332"}}></HomeNav>
-          <section classname={styles.history}>
+          <section className={styles.history}>
             <h1>Our History</h1>
-            <p></p>
+            <p>MAKA ENERGIES LIMITED was registered on the 25th of january,2023 in Zambia by the registrar of companies. With the aim of providing quality goods and services to to both local and international clients.
+              Since then the we have made in various sectors of the economy, venturing into electrical and mechanical engineering, civil construction and the 
+              supply of varios goods,currently diong business with <span>Grizzly Mining, Golden King Limited, Wideways International Limited, Hui Fan Mining Limited</span>adn various other businesses</p>
+          </section>
+          <section className={styles.policy}>
+           <h1>Our Policies</h1>
+           <PolicyGrid></PolicyGrid>
+           <Button>Details</Button>
           </section>
           <Footer></Footer>
         </main>

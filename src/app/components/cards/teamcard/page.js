5e059ffd292a805028900965391e0df285b0e77a
temwani <temwaninkowani@gmail.com>
2025-08-11 15:39:56 +0200

    change team photos, initial smtp setup

--- src/app/components/cards/teamcard/page.js
+++ src/app/components/cards/teamcard/page.js
@@ -6,7 +6,7 @@ export default function TeamCard({name,title}){
     return(
         <div className={styles.card}>
           <div className={styles.imageWrapper}>
-             <Image className={styles.image} src="/images/maka1.jpg" width={100} height={100} alt="team member image"></Image>
+             <Image className={styles.image} src="/images/person.jpg" width={100} height={100} alt="team member image"></Image>
           </div>
           <h2 className={styles.name}>{name}</h2>
           <h3 className={styles.title}>{title}</h3>

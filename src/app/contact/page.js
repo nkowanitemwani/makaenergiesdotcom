1b3ff7c9f87e7e9678274bf4796c19c2acd479ff
temwani <temwaninkowani@gmail.com>
2025-08-13 20:17:08 +0200

    minor improvements

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import styles from "./contact.module.css";
 import HomeNav from "@/app/components/navbar";
 import Footer from "@/app/components/footer";
 import ContactCards from "@/app/components/grids/contactcardgrid";
@@ -12,12 +11,12 @@ export default function ContactPage(){
     return(
         <main>
           <HomeNav style={{background : "#1a2332"}}></HomeNav>
-          <section className={styles.contact}>
+          <section>
             <h1>Get In Touch With Us</h1>
             <p>Reach out to us on any of the below contacts for quick reply</p>
             <ContactCards></ContactCards>
           </section>
-          <section id="quote" className={styles.quote}>
+          <section id="quote">
           <h1>Get a Quote</h1>
           <p>Send a us a description of the service you require and we will get back to you within 24 hours</p>
           <QuoteForm></QuoteForm>
--- src/app/team/page.js
+++ src/app/team/page.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "./team.module.css";
 import HomeNav from "@/app/components/navbar";
 import Footer from "@/app/components/footer";
 import TeamGrid from "@/app/components/grids/teamgrid";
@@ -8,7 +7,7 @@ export default function TeamPage(){
     return(
         <main>
           <HomeNav style={{background : "#1a2332"}}></HomeNav>
-          <section className={styles.team}>
+          <section>
             <h1>Meet The Maka Energies Team</h1>
             <p>Our founding members are a group of individuals with years of experience in the electrical industry and service delivery </p>
             <TeamGrid></TeamGrid>

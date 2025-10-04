5f319288cb612c508cc7232a9bc469b836841651
temwani <temwaninkowani@gmail.com>
2025-10-04 20:38:02 +0200

    change site map url

--- src/app/next-sitemap.config.js
+++ src/app/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://makaenergiesdotcom.vercel.app/',
+  siteUrl: 'https://makaenergies.org/',
   generateRobotseTxt: true,
   sitemapSize: 7000,
   changefreq: 'monthly',

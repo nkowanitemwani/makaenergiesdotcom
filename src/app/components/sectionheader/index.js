1303f200f201dd45a038911cf7a60ef5c28d7edc
temwani <temwaninkowani@gmail.com>
2025-11-07 00:47:56 +0200

    add propositions to section header component

--- src/app/components/sectionheader/index.js
+++ src/app/components/sectionheader/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './sectionheader.module.css';
 
-export default function SectionHeader({ title, subtitle, align = 'center' }) {
+export default function SectionHeader({ title, subtitle, align = 'center' , ...props}) {
   return (
-    <div className={`${styles.sectionHeader} ${styles[align]}`}>
+    <div className={`${styles.sectionHeader} ${styles[align]}`} {...props}>
       <h1>{title}</h1>
       {subtitle && <p>{subtitle}</p>}
     </div>
